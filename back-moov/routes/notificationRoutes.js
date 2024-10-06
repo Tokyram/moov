b4393c447c0123168650b6fbe920bfc255a733c2
mendrikafitahiana <ramilisonfita24@gmail.com>
2024-10-06 20:41:23 +0300

    modif

--- back-moov/routes/notificationRoutes.js
+++ back-moov/routes/notificationRoutes.js
@@ -2,8 +2,9 @@
 const express = require("express");
 const router = express.Router();
 const notificationController = require("../controllers/notificationController");
+const authMiddleware = require('../middleware/auth');
 
 router.post("/send-notification", notificationController.sendPushNotification);
-router.get("/non-lu", notificationController.countNotificationNonLu);
+router.get("/non-lu/:userId", authMiddleware, notificationController.countNotificationNonLu);
 
 module.exports = router;
